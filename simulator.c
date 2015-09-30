312aef444662533b6a462321f70da030e9fb5384
masinghv <m26singhvi@gmail.com>
2015-09-30 11:03:46 +0530

    Pthread Create

--- simulator.c
+++ simulator.c
@@ -11,18 +11,39 @@
 void* thread_one_function()
 {
   printf("Thread 1 started \n");
+  
+  unsigned int i = 0;
+  
+  pthread_t id = pthread_self();
+
+  for (i=0; i < (0xFFFFFFFF); i++)
+  ;
+
+  if (pthread_equal(id, tid[0]))
+  {
+    printf("First Thread processing\n ");
+  }
+  
   return NULL;
 }
 
 void* thread_two_function()
 {
   printf("Thread 2 started \n");
+  
+   unsigned int i = 0;
+  
+  for (i=0; i < (0xFFFFFFFF); i++)
+  ;
   return NULL;
 }
 
 void* thread_three_function()
 {
-  printf("Thread 1 started \n");
+  printf("Thread 3 started \n");
+   unsigned int i = 0;
+  for (i=0; i < (0xFFFFFFFF); i++)
+  ;
   return NULL;
 }
 
@@ -30,6 +51,9 @@ int create_thread(int num, void *func)
 {
   int err = 0;
  
+   if (func == NULL)
+    printf("Thread starting function is null\n");
+
   err = pthread_create(&(tid[num-1]), NULL, func, NULL);
 
   if (err != 0)
@@ -42,8 +66,10 @@ int create_thread(int num, void *func)
     
 int main ()
 {
-  printf("Hello This is the main thread , I am going to create three threads now \n");
+  printf("Hello This is main, I am going to create threads now \n");
   create_thread(1, &thread_one_function);
+  create_thread(2, &thread_two_function);
+  create_thread(3, &thread_three_function);
 
   return 0;
 }
